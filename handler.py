2f56b648538cc2e33aab6bcc36626f8901836f51
Oliver Steele <steele@osteele.com>
2017-10-29 14:12:44 -0400

    Connect handler to tabulator; create HTML response

--- handler.py
+++ handler.py
@@ -1,3 +1,4 @@
+import io
 import os
 import json
 
@@ -6,28 +7,49 @@ try:
 except ImportError:
     pass
 
+import tabulator
+
 
 def tabulate(event, context):
     gsheet_key = os.getenv('GOOGLE_SHEET_KEY')
     gsheet_range = os.getenv('GOOGLE_SHEET_RANGE')
+    tabulation = tabulator.tabulate(gsheet_key, gsheet_range)
 
-    body = {
-        "message": "Google sheet key={}; range={}".format(gsheet_key, gsheet_range),
-        "input": event
-    }
+    with io.StringIO() as output:
+        write_tabulation_html(tabulation, output)
+        body = output.getvalue()
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "headers": {
+            "content-type": "text/html"
+        },
+        "body": body
     }
 
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+
+def write_tabulation_html(tabulation, output):
+    counts, warnings, winners = tabulation.counts, tabulation.warnings, tabulation.winners
+    lines = []
+    output.write('Totals:\n{}\n'.format(counts.to_string()))
+    if len(winners) > 1:
+        output.write('Winners (tie): {}'.format(', '.join(winners.index)))
+    else:
+        output.write('Winner: {}'.format(winners[0]))
+    for warning in tabulation.warnings:
+        output.write(warning + "\n")
+
+
+def main():
+    gsheet_key = os.getenv('GOOGLE_SHEET_KEY')
+    gsheet_range = os.getenv('GOOGLE_SHEET_RANGE')
+    tabulation = tabulator.tabulate(gsheet_key, gsheet_range)
+    with io.StringIO() as output:
+        write_tabulation_html(tabulation, output)
+        print(output.getvalue())
+
+
+if __name__ == '__main__':
+    main()
