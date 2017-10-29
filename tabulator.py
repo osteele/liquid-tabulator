dfb0e2dca1300e5e8b3d840479182b2412b0c0aa
Oliver Steele <steele@osteele.com>
2017-10-29 11:35:21 -0400

    tabulator.py has a CLI; can be imported

--- tabulator.py
+++ tabulator.py
@@ -1,49 +1,74 @@
+"""Liquid Democracy vote tabulator.
+
+Usage:
+  tabulator.py SPREADSHEET_KEY SPREADSHEET_RANGE
+"""
+
+import sys
+from collections import namedtuple
+
 import pandas as pd
+from docopt import docopt
 
-spreadsheet_key = ''  # from the Google sheets Publish URL
-spreadsheet_range = ''  # e.g. B6:E15
-
-url = 'https://docs.google.com/spreadsheets/d/e/{}/pub?gid=0&single=true&output=csv'.format(spreadsheet_key)
-if spreadsheet_range:
-    url += '&range=' + spreadsheet_range
-
-df = pd.read_csv(url, names=['Voter', 'Vote', 'Delegate', 'Position'], index_col=0)
-
-dups = df.index.get_duplicates()
-assert not dups, "{} voted twice".format(', '.join(dups))
+Tabulation = namedtuple('Tabulation', ['counts', 'winners', 'warnings'])
 
 
-def fixed_point(f, df):
+def tabulate(spreadsheet_key, spreadsheet_range):
+    url = 'https://docs.google.com/spreadsheets/d/e/{}/pub?gid=0&single=true&output=csv'.format(
+        spreadsheet_key)
+    if spreadsheet_range:
+        url += '&range=' + spreadsheet_range
+
+    df = pd.read_csv(url, names=['Voter', 'Vote',
+                                 'Delegate', 'Position'], index_col=0)
+
+    warnings = []
+    dups = df.index.get_duplicates()
+    if dups:
+        warnings.append("{} voted twice".format(', '.join(dups)))
+
+    # Fill in missing votes by delegates' votes, or delegates' delegates' votes, etc.
+    df = fixed_point(follow_delegates, df)
+
+    # Warn (don't error) on unresolved votes.
+    # FIXME "voters" who entered neither a vote nor delegate should be reported separately
+    nas = df['Vote'].isnull().sum()
+    if nas:
+        warnings.append("{} delegated votes did not resolve".format(nas))
+
+    counts = df['Vote'].value_counts()
+    winners = counts[counts == counts[counts.idxmax()]].index
+    return Tabulation(counts=counts, warnings=warnings, winners=winners)
+
+
+def fixed_point(fn, df):
     """Find the fixed point of a DataFrame function."""
     while True:
-        df0, df = df, f(df)
+        df0, df = df, fn(df)
         if df.equals(df0):
             return df
 
 
-def follow_delegates(df):
+def follow_delegates(votes):
     """Return a new DataFrame with delegates replaced by those delegates' votes."""
     # Each voter's delegate, or themself if they didn't choose a delegate
-    delegates = df['Delegate'].fillna(df.index.to_series())
-    delegate_vote = delegates.apply(dict(df['Vote']).__getitem__)
-    return df.assign(Vote=df['Vote'].fillna(delegate_vote))
+    delegates = votes['Delegate'].fillna(votes.index.to_series())
+    delegate_vote = delegates.apply(dict(votes['Vote']).__getitem__)
+    return votes.assign(Vote=votes['Vote'].fillna(delegate_vote))
 
 
-# Fill in missing votes by delegates' votes, or delegates' delegates' votes, etc.
-df = fixed_point(follow_delegates, df)
-df
+def report_tabulation(tabulation):
+    counts, warnings, winners = tabulation.counts, tabulation.warnings, tabulation.winners
+    for warning in tabulation.warnings:
+        sys.stderr.write(warning + "\n")
+    print('Totals:\n{}\n'.format(counts.to_string()))
+    if len(winners) > 1:
+        print('Winners (tie):', ', '.join(winners.index))
+    else:
+        print('Winner:', winners[0])
 
-# Warn (don't error) on unresolved votes.
-# FIXME "voters" who entered neither a vote nor delegate should be reported separately
-nas = df['Vote'].isnull().sum()
-if nas:
-    print("{} delegated votes did not resolve".format(nas))
 
-counts = df['Vote'].value_counts()
-print('Totals:\n{}\n'.format(counts.to_string()))
-
-winners = counts[counts == counts[counts.idxmax()]].index
-if len(winners) > 1:
-    print('Winners (tie):', ', '.join(winners.index))
-else:
-    print('Winner:', winners[0])
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    tabulation = tabulate(args['SPREADSHEET_KEY'], args['SPREADSHEET_RANGE'])
+    report_tabulation(tabulation)
